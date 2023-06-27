7395c111c6fa5f4d77360916cc522a1d5dca8e7c
tranduynhat0209 <nhatducmo@gmail.com>
2023-06-27 09:43:20 +0700

    setup issuers

--- src/setup/setup.ts
+++ src/setup/setup.ts
@@ -1,5 +1,9 @@
 import { GlobalVariables } from "../common/config/global.js";
-import { ISSUER_PRIVATE_KEY, ISSUER_SERVER_URL, ZIDEN_SERVER_URI } from "../common/config/secrets.js";
+import {
+  ISSUER_PRIVATE_KEY,
+  ISSUER_SERVER_URL,
+  ZIDEN_SERVER_URI,
+} from "../common/config/secrets.js";
 import Issuer from "../models/Issuer.js";
 import { utils as zidenjsUtils } from "@zidendev/zidenjs";
 import { registerIssuer } from "../services/TreeState.js";
@@ -10,95 +14,100 @@ import SchemaRegistry from "../models/SchemaRegistry.js";
 import axios from "axios";
 
 export async function setupIssuer() {
-    try {
-        for (let i = 0; i < ISSUER_PRIVATE_KEY.length; i++) {
-            const privateKey = ISSUER_PRIVATE_KEY[i];
-            const privateKey2Buf = zidenjsUtils.hexToBuffer(privateKey, 32);
-    
-            const pubkeyX = GlobalVariables.F.toObject(GlobalVariables.eddsa.prv2pub(privateKey2Buf)[0]).toString(10);
-            const pubkeyY = GlobalVariables.F.toObject(GlobalVariables.eddsa.prv2pub(privateKey2Buf)[1]).toString(10);
-    
-            const issuer = await Issuer.findOne({pubkeyX: pubkeyX, pubkeyY: pubkeyY});
-            if (!issuer) {
-                await registerIssuer(privateKey);
-                
-            }
-        }
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    for (let i = 0; i < ISSUER_PRIVATE_KEY.length; i++) {
+      const privateKey = ISSUER_PRIVATE_KEY[i];
+      const privateKey2Buf = zidenjsUtils.hexToBuffer(privateKey, 32);
 
+      const pubkeyX = GlobalVariables.F.toObject(
+        GlobalVariables.eddsa.prv2pub(privateKey2Buf)[0]
+      ).toString(10);
+      const pubkeyY = GlobalVariables.F.toObject(
+        GlobalVariables.eddsa.prv2pub(privateKey2Buf)[1]
+      ).toString(10);
+
+      const issuer = await Issuer.findOne({
+        pubkeyX: pubkeyX,
+        pubkeyY: pubkeyY,
+      });
+      if (!issuer) {
+        await registerIssuer(privateKey);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function setupSchemaRegistry() {
-    try {
-        const schemaJsons = JSON.parse(fs.readFileSync('setup/schema.json', 'utf-8'));
-        const issuerList = await Issuer.find();
-        let issuerIds = [];
-        for (let i = 0; i <issuerList.length; i++) {
-            if (issuerList[i].issuerId != null) {
-                issuerIds.push(issuerList[i].issuerId);
-            }
-        }
-
-        for (let i = 0; i < schemaJsons.length; i++) {
-            const schemaJson = schemaJsons[i];
-            try {
-                const schema = await createNewSchema(schemaJson);
-                for (let j = 0; j < issuerList.length; j++) {
-                    const registry = await SchemaRegistry.findOne({"schemaHash": schema["@hash"], "issuerId": issuerList[i].issuerId});
-                    if (registry != undefined) {
-                        continue;
-                    }
-    
-                    await createNewRegistry(
-                        schema["@hash"]!,
-                        issuerList[i].issuerId!,
-                        `${schema["@name"]}`,
-                        30*24*60*60*60*1000,
-                        true,
-                        97,
-                        `${ISSUER_SERVER_URL}/api/v1`
-                    );
-                }
-    
-            } catch (err) {
-            }
-            
-        }
-
-        const serviceExisted = (await axios.get(`${ZIDEN_SERVER_URI}/api/v1/services`)).data.services;
-        const services = JSON.parse(fs.readFileSync('setup/service.json', 'utf-8'));
-        for (let i = 0; i < services.length; i++) {
-            let ok = 1;
-            for (let j = 0; j < serviceExisted.length; j++) {
-                if (services[i].name == serviceExisted[j].name) {
-                    ok = 0;
-                    break;
-                }
-            }
-            if (ok == 1) {
-                for (let j = 0; j < services[i].requirements; j++) {
-                    if (services[i].requirements[j].allowedIssuers.length == 0) {
-                        services[i].requirements[j].allowedIssuers.length = issuerIds;
-                    }
-                }
-
-                try {
-                    await axios.request({
-                        url: `${ZIDEN_SERVER_URI}/api/v1/services`,
-                        method: "post",
-                        data: services[i]
-                    })
-                } catch (err) {
-                    console.log(err)
-                }
-            }
-            
-            
-        }
-    } catch (err) {
-        console.log(err);
+  try {
+    const schemaJsons = JSON.parse(
+      fs.readFileSync("setup/schema.json", "utf-8")
+    );
+    const issuerList = await Issuer.find();
+    let issuerIds = [];
+    for (let i = 0; i < issuerList.length; i++) {
+      if (issuerList[i].issuerId != null) {
+        issuerIds.push(issuerList[i].issuerId);
+      }
     }
 
+    for (let i = 0; i < schemaJsons.length; i++) {
+      const schemaJson = schemaJsons[i];
+      try {
+        const schema = await createNewSchema(schemaJson);
+
+        const registry = await SchemaRegistry.findOne({
+          schemaHash: schema["@hash"],
+          issuerId: issuerList[i].issuerId,
+        });
+        if (registry != undefined) {
+          continue;
+        }
+
+        await createNewRegistry(
+          schema["@hash"]!,
+          issuerList[i].issuerId!,
+          `${schema["@name"]}`,
+          30 * 24 * 60 * 60 * 60 * 1000,
+          true,
+          97,
+          `${ISSUER_SERVER_URL}/api/v1`
+        );
+      } catch (err) {}
+    }
+
+    // const serviceExisted = (
+    //   await axios.get(`${ZIDEN_SERVER_URI}/api/v1/services`)
+    // ).data.services;
+    // const services = JSON.parse(fs.readFileSync("setup/service.json", "utf-8"));
+    // for (let i = 0; i < services.length; i++) {
+    //   let ok = 1;
+    //   for (let j = 0; j < serviceExisted.length; j++) {
+    //     if (services[i].name == serviceExisted[j].name) {
+    //       ok = 0;
+    //       break;
+    //     }
+    //   }
+    //   if (ok == 1) {
+    //     for (let j = 0; j < services[i].requirements; j++) {
+    //       if (services[i].requirements[j].allowedIssuers.length == 0) {
+    //         services[i].requirements[j].allowedIssuers.length = issuerIds;
+    //       }
+    //     }
+
+    //     try {
+    //       await axios.request({
+    //         url: `${ZIDEN_SERVER_URI}/api/v1/services`,
+    //         method: "post",
+    //         data: services[i],
+    //       });
+    //     } catch (err) {
+    //       console.log(err);
+    //     }
+    //   }
+    // }
+  } catch (err) {
+    console.log(err);
+  }
 }
